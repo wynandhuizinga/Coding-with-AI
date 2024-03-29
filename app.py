af3ca285b24b90055b5f50e3f21494dc223028de
wynandhuizinga <wynandhuizinga@gmail.com>
2024-03-29 11:52:40 +0100

    tossing overboard some front end security measures. Updating backend messages. Changing to port 80 by default. Indents update

--- app.py
+++ app.py
@@ -21,12 +21,11 @@ def get_all_assets():
   for asset in assets:
    asset_dict = {'id': asset.id, 'manufacturer': asset.manufacturer, 'name': asset.name, 'amount': asset.amount}
    asset_list.append(asset_dict)
-  return jsonify({'message': asset_list}), 201
+  return jsonify(asset_list), 201, {"Access-Control-Allow-Origin": "*"}
 
 @app.route('/total_gwp', methods=['GET'])
 def get_gwp_totals():
   asset_ids = Asset.query.all()
-#  gwp_totals = {}
   gwp_totals = 0.0
   for asset in asset_ids:
       reference_device = ReferenceDevice.query.filter_by(manufacturer=asset['manufacturer'], name=asset['name']).first()
@@ -34,7 +33,7 @@ def get_gwp_totals():
           gwp_totals += reference_device.gwp_total * asset.amount / reference_device.lifetime
   km_driven_comp = gwp_totals * float(4.18429) # source: https://www.epa.gov/energy/greenhouse-gas-equivalencies-calculator --> converted 2.6 miles to km (=4.18...)
   message = str("Total global warming potential of your CMDB: ") + str(round(gwp_totals, 2)) + str(" kgCO2eq over the entire lifetime of all assets combined.") + str("\n This is equivalent to ") + str(round(km_driven_comp, 2)) + str(" km driving with a personnel vehicle")
-  return jsonify({'message': message}), 201
+  return jsonify(message), 201, {"Access-Control-Allow-Origin": "*"}
 
 @app.route('/assets', methods=['POST'])
 def create_new_asset():
@@ -42,7 +41,7 @@ def create_new_asset():
     asset = Asset(manufacturer=data['manufacturer'], name=data['name'], amount=data['amount'], )
     db.session.add(asset)
     db.session.commit()
-    return jsonify({'message': 'Asset created successfully.'}), 201
+    return jsonify({'message': 'Asset created successfully.'}), 201, {"Access-Control-Allow-Origin": "*"}
 
 @app.route('/reference_devices', methods=['GET'])
 def get_reference_devices():
@@ -69,10 +68,10 @@ def get_reference_devices():
                gwp_othercomponents_ratio=float(row['gwp_othercomponents_ratio']))
            db.session.add(new_device)
            db.session.commit()
-       return jsonify({"message": "OK"}), 201
+       return jsonify({"message": "OK"}), 201, {"Access-Control-Allow-Origin": "*"}
    except Exception as e:
        print("An error occurred: ", str(e))
-       return jsonify({"message": "FAILED"}), 500
+       return jsonify({"message": "FAILED"}), 500, {"Access-Control-Allow-Origin": "*"}
 
 @app.route('/refresh_reference_devices', methods=['GET'])
 def get_refresh_reference_devices():
@@ -81,7 +80,7 @@ def get_refresh_reference_devices():
    csv_data = response.text
    with codecs.open('referencedevices.csv', 'w') as f:
        f.write(csv_data)
-   return jsonify({"message": "OK"}), 201
+   return jsonify({"message": "OK"}), 201, {"Access-Control-Allow-Origin": "*"}
 
 if __name__ == '__main__':
-  app.run(port=6000)
+  app.run(port=80)
--- web/index.js
+++ web/index.js
@@ -1,35 +1,35 @@
 function getAssets() {
-   fetch("/assets")
-   .then(response => response.json())
-   .then(data => {
-       const container = document.querySelector(".container");
-       data.forEach(item => {
-           const card = document.createElement("div");
-           card.classList.add("card");
-           const title = document.createElement("h3");
-           title.textContent = item.name;
-           card.appendChild(title);
-           const info = document.createElement("p");
-           info.textContent = `ID: ${item.id}, Manufacturer: ${item.manufacturer}, Amount: ${item.amount}`;
-           card.appendChild(info);
-           container.appendChild(card);
-       });
-       const totalCountElement = document.getElementById("total-count");
-       totalCountElement.textContent = `${data.length} Total Count`;
-   })
-   .catch(error => console.log(error));
+  fetch("http://localhost:80/assets")
+  .then(response => response.json())
+  .then(data => {
+      const container = document.querySelector(".container");
+      data.forEach(item => {
+          const card = document.createElement("div");
+          card.classList.add("card");
+          const title = document.createElement("h3");
+          title.textContent = item.name;
+          card.appendChild(title);
+          const info = document.createElement("p");
+          info.textContent = `ID: ${item.id}, Manufacturer: ${item.manufacturer}, Amount: ${item.amount}`;
+          card.appendChild(info);
+          container.appendChild(card);
+      });
+      const totalCountElement = document.getElementById("total-count");
+      totalCountElement.textContent = `${data.length} Total Count`;
+  })
+  .catch(error => console.log(error));
 }
 
 function getGWP() {
-   fetch("/total_gwp")
-   .then(response => response.json())
-   .then(data => {
-       const messageContainer = document.querySelector("#message-container");
-       const message = document.createElement("p");
-       message.textContent = data.message;
-       messageContainer.appendChild(message);
-   })
-   .catch(error => console.log(error));
+  fetch("http://localhost:80/total_gwp")
+  .then(response => response.json())
+  .then(data => {
+      const messageContainer = document.querySelector("#message-container");
+      const message = document.createElement("p");
+      message.textContent = data.message;
+      messageContainer.appendChild(message);
+  })
+  .catch(error => console.log(error));
 }
 
 function handleResponse(data) {
